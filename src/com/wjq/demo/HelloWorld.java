e836fcb1b6104ca1a86988da10441c1492997f6f
wangjunqiang <wangjunqiang@wecash.net>
2017-07-02 19:37:12 +0800

    Initial commit

---
+++ src/com/wjq/demo/HelloWorld.java
@@ -0,0 +1,19 @@
+package com.wjq.demo;
+
+/**
+ * @author wjq
+ * @ClassName : ${type_name}
+ * @Version 1.0
+ * @ModifiedBy wjq
+ * @date ${date} ${time}
+ */
+public class HelloWorld {
+
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+    }
+
+
+
+}
