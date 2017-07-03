c78bc2195495b2e058feec90f9454d35263ca91d
wangjunqiang <wangjunqiang@wecash.net>
2017-07-03 13:12:01 +0800

    a

--- src/com/wjq/demo/HelloWorld.java
+++ src/com/wjq/demo/HelloWorld.java
@@ -10,7 +10,7 @@ package com.wjq.demo;
 public class HelloWorld {
 
     public static void main(String[] args){
-        System.out.println("Hello World!");
+        System.out.println("Hello World!!");
         System.out.println("Hello World!");
     }
 
